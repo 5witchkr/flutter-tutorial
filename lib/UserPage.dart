f4c76e495c9607f0b97edb5f5d9df40c3693045f
5witchkr <hob4419@gmail.com>
2022-05-08 14:32:47 +0900

    provider2

--- lib/UserPage.dart
+++ lib/UserPage.dart
@@ -11,13 +11,19 @@ class UserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       //provider Store1.name data사용
-      appBar: AppBar(title: Text(context.watch<Store1>().name),),
-      body: Column(
+      appBar: AppBar(title: Text(context.watch<Store2>().name),),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.grey,
+          ),
+          Text('팔로워 ${context.watch<Store1>().follower}명'),
           ElevatedButton(onPressed: () {
             //provider Store1 funtion 사용
-            context.read<Store1>().changeName();
-          }, child: Text('버튼'))
+            context.read<Store1>().addFollower();
+          }, child: Text('팔로우'))
         ],
       ),
     );
